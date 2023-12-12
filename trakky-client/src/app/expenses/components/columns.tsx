63832b4946c1abff0f113d779f173621a3032297
giuseppe-ro <info@rainingdreams.co.uk>
2023-12-12 08:12:59 +0000

    layout improvements

--- trakky-client/src/app/expenses/components/columns.tsx
+++ trakky-client/src/app/expenses/components/columns.tsx
@@ -2,6 +2,8 @@ import { ColumnDef } from "@tanstack/react-table";
 import { Payment } from "@/infrastructure/payment.tsx";
 import { formatCurrency, formatDate } from "@/lib/formatter.ts";
 import { fuzzySort } from "@/lib/filters.ts";
+import { EditCell } from "@/app/expenses/components/edit-cell.tsx";
+import { PaymentForm } from "@/app/expenses/components/payment-form.tsx";
 
 export const ColumnDefinition: ColumnDef<Payment, number | string>[] = [
   {
@@ -50,12 +52,31 @@ export const ColumnDefinition: ColumnDef<Payment, number | string>[] = [
       return <div className="font-sm">{row.getValue("description")}</div>;
     },
   },
+  {
+    id: 'edit',
+    header: () => null,
+    cell: ({ row }) => {
+      return (
+        <td>
+          <EditCell id={(row.original as Payment).id}>
+            <PaymentForm
+              editValues={row.original as Payment}
+              refresh={() => void 0}
+              title={"Edit Transaction"}
+            ></PaymentForm>
+          </EditCell>
+        </td>
+      )
+    },
+  },
 ];
 
 export const colSize = (id: string): number | string => {
   switch (id) {
     case "description":
       return "auto";
+    case "edit":
+      return 20;
     case "date":
       return 90;
     case "type" || "owner":

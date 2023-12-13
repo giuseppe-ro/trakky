7c32ddc085a1704868262b62ea95c3f00490cbb9
giuseppe-ro <info@rainingdreams.co.uk>
2023-12-13 13:33:49 +0000

    bigger margins

--- trakky-client/src/components/ui/containers.tsx
+++ trakky-client/src/components/ui/containers.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function PageContainer({ children, className }: { children: React.ReactNode, className?: string }) {
   return (
-    <div className={`md:container px-0 pb-2 md:px-6 md:pb-2 mx-auto w-full ${className}`}>
+    <div className={`md:container px-0 pb-2 md:px-12 mx-auto w-full ${className}`}>
       <>
         {children}
       </>

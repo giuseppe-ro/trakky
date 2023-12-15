19510f7781ac831bf5c345998fd78ed9ea831036
giuseppe-ro <info@rainingdreams.co.uk>
2023-12-15 08:21:31 +0000

    testing demo mode

--- trakky-client/src/constants.ts
+++ trakky-client/src/constants.ts
@@ -1,3 +1,12 @@
 export const serverUrl = "http://trakky-api.localdomain";
 
-export const demoMode = true;
+export const demoMode = (): boolean => {
+  let demoMode = false;
+  try {
+    demoMode = process.env.REACT_APP_DEMO_MODE === "true";
+  } catch (e) {
+    demoMode = false;
+  }
+
+  return demoMode;
+};

4c0e035ae9882a8e88b22c4c75e2fbab8849e07e
giuseppe-ro <info@rainingdreams.co.uk>
2024-10-19 17:22:45 +0100

    charts are now hidden when data is not enough (#194)
    Co-authored-by: Joe85gr <info@rainingdreams.co.uk>

--- trakky-client/src/app/dashboards/components/dashboards.tsx
+++ trakky-client/src/app/dashboards/components/dashboards.tsx
@@ -1,6 +1,7 @@
 import { SubTitle } from '@/components/ui/text';
 import { PaymentOverview } from '@/models/payment-overview';
 import { OwnerOverview } from '@/models/owner-overview';
+import { twMerge } from 'tailwind-merge';
 import { ExpensesDashboard, ExpensesPieChart, UsersDashboard } from './charts';
 
 interface DashboardsProps {
@@ -23,11 +24,23 @@ function Dashboards({
     <>
       <div className="mt-6 text-center mr-4 lg:mx-0">
         <div className="lg:grid gap-4 lg:grid-cols-2">
-          <div className="mt-4 sm:mt-0" title="Expenses Dashboard">
+          <div
+            className={twMerge(
+              'mt-4 sm:mt-0 block',
+              paymentOverviews.length < 2 && 'hidden'
+            )}
+            title="Expenses Dashboard"
+          >
             <SubTitle title="Expenses" {...{ className: 'ml-16' }} />
             <ExpensesDashboard data={paymentOverviews} />
           </div>
-          <div className="mt-4 md:mt-0" title="Users Dashboard">
+          <div
+            className={twMerge(
+              'mt-4 md:mt-0 block',
+              ownersOverview.length < 2 && 'hidden'
+            )}
+            title="Users Dashboard"
+          >
             <SubTitle
               title="Users Comparison"
               {...{ className: 'mb-4 ml-16' }}
